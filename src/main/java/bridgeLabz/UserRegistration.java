2d9d2a583bedbcbc95a604012630527167d1251f
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-05-29 10:44:39 +0530

    UserRegestration

---
+++ src/main/java/bridgeLabz/UserRegistration.java
@@ -0,0 +1,61 @@
+package bridgeLabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public boolean validateFirstName(String name) {
+
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches())
+            return true;
+        else
+            return false;
+    }
+    public boolean validateLastName(String name) throws InvalidvalidateLastName{
+
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches())
+            return true;
+        else
+            return false;
+    }
+    public boolean validateEmail(String eMail) {
+
+        System.out.print("Enter Email id:- ");
+        Pattern pattern2 = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+        Matcher matcher2 = pattern2.matcher(eMail);
+        if (matcher2.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean validateMobileNumber(String mobileNumber) {
+
+        System.out.print("Enter Mobile number:- ");
+        Pattern pattern3 = Pattern.compile("^(91)?[7-9][0-9]{9}$");
+        Matcher matcher3 = pattern3.matcher(mobileNumber);
+        if (matcher3.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean validatePassword(String password) {
+
+        System.out.print("Enter the Password:- ");
+
+        //(?=.*[A-Z])(?=.*[0-9])(?=.*
+        Pattern pattern4 = Pattern.compile("^(?=[a-z]*[A-Z])(?=.*[0-9]).{8,}$");
+        Matcher matcher4 = pattern4.matcher(password);
+        if (matcher4.matches())
+            return true;
+        else
+            return false;
+
+
+    }
+}
---
+++ src/test/java/bridgeLabz/UserRegistrationTest.java
@@ -0,0 +1,73 @@
+package bridgeLabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    static UserRegistration userRegistration = new UserRegistration();
+
+
+    @BeforeAll
+    public static void init() {
+        System.out.println("Before all");
+        userRegistration = new UserRegistration();
+    }
+    @Test
+    void givenFirstNameShouldReturnTrue() throws InvalidFirstNameShouldReturnTrue{
+        boolean actualValue = userRegistration.validateFirstName("Vamshi");
+        Assertions.assertTrue(actualValue);
+        System.out.println("FirstName Successfully Validate.");
+    }
+    @Test
+    void givenLastNameReturnTrue() throws InvalidLastNameReturnTrue{
+        boolean actualFirstName = userRegistration.validateLastName("Reddy");
+        Assertions.assertTrue(actualFirstName);
+        System.out.println("LastName Successfully Validate.");
+    }
+    @Test
+    void givenEmailRerurnTrue() throws InvalidEmailRerurnTrue{
+        boolean actualEmail = userRegistration.validateEmail("Vamshireddy2@gmail.com");
+        Assertions.assertTrue(actualEmail);
+        System.out.println("Email Successfully Validate.");
+    }
+    @Test
+    void givenMobileNumberRerurnTrue() throws InvalidMobileNumberRerurnTrue{
+        boolean actualMobileNumber = userRegistration.validateMobileNumber("7730006563");
+        Assertions.assertTrue(actualMobileNumber);
+        System.out.println("Mobile Number Successfully Validate.");
+    }
+    @Test
+    void givenPasswordRerurnTrue() throws InvalidPasswordRerurnTrue{
+        boolean actualMobileNumber = userRegistration.validatePassword("vamshI@9");
+        Assertions.assertTrue(actualMobileNumber);
+        System.out.println("Password Successfully Validate.");
+    }
+    @Test
+    void checkEmailShouldReturnTrue() {
+        String text = "abc@yahoo.com";
+        boolean email = userRegistration.validateEmail(text);
+        Assertions.assertTrue(email);
+        String text1 = "abc@yahoo.com";
+        boolean email1 = userRegistration.validateEmail(text1);
+        Assertions.assertTrue(email1);
+        String text2 = "abc@yahoo.com";
+        boolean email2 = userRegistration.validateEmail(text2);
+        Assertions.assertTrue(email2);
+        String text3 = "abc@yahoo.com";
+        boolean email3 = userRegistration.validateEmail(text3);
+        Assertions.assertTrue(email3);
+        String text4 = "abc@yahoo.com";
+        boolean email4 = userRegistration.validateEmail(text4);
+        Assertions.assertTrue(email4);
+        String text5 = "abc@yahoo.com";
+        boolean email5 = userRegistration.validateEmail(text5);
+        Assertions.assertTrue(email5);
+        String text6 = "abc@yahoo.com";
+        boolean email6 = userRegistration.validateEmail(text6);
+        Assertions.assertTrue(email6);
+
+        System.out.println("All test case successfully PasseD!!!!");
+
+    }
+}
